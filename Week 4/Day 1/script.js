1abc830b857b139ad9662fe63c7219763b0007a4
zahraa <zahraafaq1@gmail.com>
2026-07-20 17:14:35 +0500

    week4day1 js

---
+++ Week 4/Day 1/script.js
@@ -0,0 +1,87 @@
+// ===============================
+// Variables
+// ===============================
+
+let name = "Zahra Afaq";
+
+let age = 21;
+
+let isStudent = true;
+
+
+// Display variables
+
+console.log("Name:", name);
+
+console.log("Age:", age);
+
+console.log("Student:", isStudent);
+
+
+// ===============================
+// Data Types
+// ===============================
+
+let city = "Lahore";
+
+let semester = 6;
+
+let learning = true;
+
+let temperature = 34.5;
+
+console.log("City:", city);
+
+console.log("Semester:", semester);
+
+console.log("Learning Web Development:", learning);
+
+console.log("Temperature:", temperature);
+
+
+// ===============================
+// Arithmetic Operators
+// ===============================
+
+let a = 10;
+
+let b = 3;
+
+console.log("Addition:", a + b);
+
+console.log("Subtraction:", a - b);
+
+console.log("Multiplication:", a * b);
+
+console.log("Division:", a / b);
+
+console.log("Remainder:", a % b);
+
+
+// ===============================
+// More Variable Examples
+// ===============================
+
+const university = "COMSATS University Islamabad";
+
+console.log("University:", university);
+
+
+// ===============================
+// Changing a let Variable
+// ===============================
+
+let score = 80;
+
+console.log("Original Score:", score);
+
+score = 95;
+
+console.log("Updated Score:", score);
+
+
+// const cannot be changed
+
+const country = "Pakistan";
+
+console.log("Country:", country);
